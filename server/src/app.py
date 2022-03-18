7e5be94b223e96d243f03b2ce5b1a26f08dcde37
KJHJason <kuanjunhaojason@gmail.com>
2022-03-19 00:29:37 +0800

    first commit

---
+++ server/src/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, abort, request, make_response
+import requests, time, os
+
+app = Flask(__name__)
+
+GOOGLE_API_KEY = "AIzaSyBch3wqIUlW5SDacOT7yAzeJRe2Jh4NsEA"
+
+def format_server_time():
+  server_time = time.localtime()
+  return time.strftime("%I:%M:%S %p", server_time)
+
+@app.route("/")
+def index():
+	context = { "server_time": format_server_time() }
+	return render_template("home.html", context=context)
+
+@app.post("/query")
+def query():
+	reqData = ""
+	queryID = request.json.get("id")
+	gdriveType = request.json.get("type")
+	if gdriveType == "file" and len(queryID) > 10:
+		req = requests.get(f"https://www.googleapis.com/drive/v3/files/{queryID}?key={GOOGLE_API_KEY}")
+		if req.status_code != 200:
+			abort(429)
+		else: 
+			reqData = req.json()
+	elif gdriveType == "folder" and len(queryID) > 10:
+		req = requests.get(f"https://www.googleapis.com/drive/v3/files?q=%27{queryID}%27+in+parents&key={GOOGLE_API_KEY}")
+		if req.status_code != 200:
+			abort(429)
+		else: 
+			reqData = req.json()
+	else:
+		abort(403)
+	return make_response(reqData, 200)
+
+if __name__ == "__main__":
+	app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
