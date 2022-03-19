b23b33154585add27f2af86afd1d275eea683f47
KJHJason <kuanjunhaojason@gmail.com>
2022-03-19 11:35:26 +0800

    added abort 404 if not valid

--- server/src/app.py
+++ server/src/app.py
@@ -1,34 +1,40 @@
 from flask import Flask, render_template, abort, request, make_response
-import requests, time, os
+from os import environ
+import requests, time
 
 app = Flask(__name__)
+
+DEBUG_MODE = False
 
 GOOGLE_API_KEY = "AIzaSyBch3wqIUlW5SDacOT7yAzeJRe2Jh4NsEA"
 
 def format_server_time():
-	server_time = time.localtime()
-	return time.strftime("%I:%M:%S %p", server_time)
+	serverTime = time.localtime()
+	return time.strftime("%I:%M:%S %p", serverTime)
 
 @app.route("/")
 def index():
 	context = { "server_time": format_server_time() }
 	return render_template("home.html", context=context)
 
-@app.post("/query")
+@app.route("/query", methods=["GET", "POST"])
 def query():
-	reqData = ""
-	dataPayload = request.json.get("data")
-	queryID = dataPayload.get("id")
-	gdriveType = dataPayload.get("type")
-	if gdriveType == "file":
-		req = requests.get(f"https://www.googleapis.com/drive/v3/files/{queryID}?key={GOOGLE_API_KEY}")
-		reqData = req.json()
-	elif gdriveType == "folder":
-		req = requests.get(f"https://www.googleapis.com/drive/v3/files?q=%27{queryID}%27+in+parents&key={GOOGLE_API_KEY}")
-		reqData = req.json()
+	if request.method == "POST":
+		reqData = ""
+		dataPayload = request.json.get("data")
+		queryID = dataPayload.get("id")
+		gdriveType = dataPayload.get("type")
+		if gdriveType == "file":
+			req = requests.get(f"https://www.googleapis.com/drive/v3/files/{queryID}?key={GOOGLE_API_KEY}")
+			reqData = req.json()
+		elif gdriveType == "folder":
+			req = requests.get(f"https://www.googleapis.com/drive/v3/files?q=%27{queryID}%27+in+parents&key={GOOGLE_API_KEY}")
+			reqData = req.json()
+		else:
+			abort(404)
+		return make_response(reqData, 200)
 	else:
-		abort(403)
-	return make_response(reqData, 200)
+		abort(404)
 
 if __name__ == "__main__":
-	app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+	app.run(debug=DEBUG_MODE, host="0.0.0.0", port=int(environ.get("PORT", 8080)))
