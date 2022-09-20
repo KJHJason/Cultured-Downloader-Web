84b955b91c4d19d439860e2efc6706faa1aa2899
KJHJason <kuanjunhaojason@gmail.com>
2022-09-20 21:33:27 +0800

    bug fixes

--- src/functions/middleware.py
+++ src/functions/middleware.py
@@ -95,8 +95,8 @@ def add_middleware_to_app(app: ASGIApp):
                 CacheControlURLRule(path=re.compile(r"^/static/.*$"), cache_control=ONE_YEAR_CACHE),
                 CacheControlURLRule(path=re.compile(r"^/api/v\d+/redoc$"), cache_control=ONE_DAY_CACHE),
                 CacheControlURLRule(path=re.compile(r"^/api/v\d+/openapi\.json$"), cache_control=ONE_DAY_CACHE),
-                CacheControlURLRule(path=re.compile(r"^/api/v\d+/software/latest/file$", cache_control=THIRTY_MIN_CACHE)),
-                CacheControlMiddleware(path=re.compile(r"^/api/v\d+/software/latest/version$", cache_control=ONE_DAY_CACHE)),
+                CacheControlURLRule(path=re.compile(r"^/api/v\d+/software/latest/file$"), cache_control=THIRTY_MIN_CACHE),
+                CacheControlURLRule(path=re.compile(r"^/api/v\d+/software/latest/version$"), cache_control=ONE_DAY_CACHE),
             )
         )
 
